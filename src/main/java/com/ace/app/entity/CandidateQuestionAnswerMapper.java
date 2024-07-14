526dfa20e845a50b8f0c2505d680906d6f820147
Crosslywere <ogboruj@outlook.com>
2024-07-14 09:27:06 +0100

    Created Id class

---
+++ src/main/java/com/ace/app/entity/CandidateQuestionAnswerMapper.java
@@ -0,0 +1,85 @@
+package com.ace.app.entity;
+
+import com.ace.app.model.CandidateQuestionAnswerMapperId;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Ogboru Jude
+ * @version 0.0.1-SNAPSHOT
+ */
+@Getter
+@Setter
+@Entity
+@Table( name = "candidate_question_answer_mapper" )
+@NoArgsConstructor
+@AllArgsConstructor
+public class CandidateQuestionAnswerMapper {
+
+	@EmbeddedId
+	private CandidateQuestionAnswerMapperId candidateQuestionAnswerId;
+
+	@ManyToOne
+	private Question question;
+
+	private Integer answerIndex;
+
+	public CandidateQuestionAnswerMapper( Integer candidateQuestionNumber, Candidate candidate, Paper paper, Question question ) {
+		this.candidateQuestionAnswerId = new CandidateQuestionAnswerMapperId( candidateQuestionNumber, candidate, paper );
+		this.question = question;
+	}
+
+	public CandidateQuestionAnswerMapper( Integer candidateQuestionNumber, Candidate candidate, Question question ) {
+		this.candidateQuestionAnswerId = new CandidateQuestionAnswerMapperId( candidateQuestionNumber, candidate, question.getPaper() );
+		this.question = question;
+	}
+
+	public Integer getCandidateQuestionNumber() {
+		if ( candidateQuestionAnswerId == null ) {
+			return null;
+		}
+		return candidateQuestionAnswerId.getCandidateQuestionNumber();
+	}
+
+	public void setCandidateQuestionNumber( Integer candidateQuestionNumber ) {
+		if ( candidateQuestionAnswerId == null ) {
+			candidateQuestionAnswerId = new CandidateQuestionAnswerMapperId();
+		}
+		candidateQuestionAnswerId.setCandidateQuestionNumber( candidateQuestionNumber );
+	}
+
+	public Paper getPaper() {
+		if ( candidateQuestionAnswerId == null ) {
+			return null;
+		}
+		return candidateQuestionAnswerId.getPaper();
+	}
+
+	public void setPaper( Paper paper ) {
+		if ( candidateQuestionAnswerId == null ) {
+			candidateQuestionAnswerId = new CandidateQuestionAnswerMapperId();
+		}
+		candidateQuestionAnswerId.setPaper( paper );
+	}
+
+	public Candidate getCandidate() {
+		if ( candidateQuestionAnswerId == null ) {
+			return null;
+		}
+		return candidateQuestionAnswerId.getCandidate();
+	}
+
+	public void setCandidate( Candidate candidate ) {
+		if ( candidateQuestionAnswerId == null ) {
+			candidateQuestionAnswerId = new CandidateQuestionAnswerMapperId();
+		}
+		candidateQuestionAnswerId.setCandidate( candidate );
+	}
+}
