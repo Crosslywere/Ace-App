113098bd473bac8cb747a8a2f382153933ecba0e
Crosslywere <ogboruj@outlook.com>
2024-07-30 21:25:35 +0100

    Cleaned up code

--- src/main/java/com/ace/app/repository/CandidateRepository.java
+++ src/main/java/com/ace/app/repository/CandidateRepository.java
@@ -1,9 +1,12 @@
 package com.ace.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Modifying;
+// import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ace.app.entity.Candidate;
+// import com.ace.app.entity.Paper;
 import com.ace.app.model.CandidateId;
 
 /**
@@ -12,4 +15,8 @@ import com.ace.app.model.CandidateId;
  */
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, CandidateId> {
+
+	// @Modifying
+	// @Query( "insert into candidate.answerMapper " )
+	// void saveAnswerIndex( CandidateId candidateId, Paper paper, Integer number, Integer answerIndex );
 }
