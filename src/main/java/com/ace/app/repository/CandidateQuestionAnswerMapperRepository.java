c16c876390c66e1c249ecd35ad721262b21570b1
Crosslywere <ogboruj@outlook.com>
2024-09-17 10:11:24 +0100

    Added default methods for findById

--- src/main/java/com/ace/app/repository/CandidateQuestionAnswerMapperRepository.java
+++ src/main/java/com/ace/app/repository/CandidateQuestionAnswerMapperRepository.java
@@ -1,11 +1,20 @@
 package com.ace.app.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ace.app.entity.Candidate;
 import com.ace.app.entity.CandidateQuestionAnswerMapper;
+import com.ace.app.entity.Paper;
 import com.ace.app.model.CandidateQuestionAnswerMapperId;
 
 @Repository
 public interface CandidateQuestionAnswerMapperRepository extends JpaRepository<CandidateQuestionAnswerMapper, CandidateQuestionAnswerMapperId> {
+
+	default Optional<CandidateQuestionAnswerMapper> findById( Integer questionNumber, Candidate candidate, Paper paper ) {
+		return findById( new CandidateQuestionAnswerMapperId( questionNumber, candidate, paper ) );
+	}
+
 }
--- src/main/java/com/ace/app/repository/PaperRepository.java
+++ src/main/java/com/ace/app/repository/PaperRepository.java
@@ -1,8 +1,11 @@
 package com.ace.app.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ace.app.entity.Exam;
 import com.ace.app.entity.Paper;
 import com.ace.app.model.PaperId;
 
@@ -12,4 +15,8 @@ import com.ace.app.model.PaperId;
  */
 @Repository
 public interface PaperRepository extends JpaRepository<Paper, PaperId> {
+
+	default Optional<Paper> findById( String paperName, Exam exam ) {
+		return findById( new PaperId( exam, paperName ) );
+	}
 }
